7564d962280928a4db3d61372e3616041f8950f0
Pablo Flores <paflores@proveedor.teco.com.ar>
2025-08-04 00:37:58 -0300

    chore(ci): update karma.config.json to generate lcov.info

--- projects/ui-kit/karma.conf.js
+++ projects/ui-kit/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "../../coverage/ui-kit"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [{ type: "html" }, { type: "text-summary" }, { type: 'lcovonly' }],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
